09528175e50fc5a33a344177df72d549777348fd
wxnyeki <frankwanyeki01@gmail.com>
2026-04-08 09:09:11 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,34 @@
+function calculateTax(amount) {
+  return amount * 0.10;
+}
 
+
+function convertToUpperCase(text) {
+  return text.toUpperCase();
+}
+
+
+function findMaximum(num1, num2) {
+  if (num1 > num2) {
+    return num1;
+  } else {
+    return num2;
+  }
+}
+
+
+function isPalindrome(word) {
+  let reversed = word.split('').reverse().join('');
+  return word === reversed;
+}
+
+function calculateDiscountedPrice(originalPrice, discountPercentage) {
+  let discount = originalPrice * (discountPercentage / 100);
+  return originalPrice - discount;
+}
+
+
+module.exports = { calculateTax, convertToUpperCase, findMaximum, isPalindrome, calculateDiscountedPrice };
 
 
 
